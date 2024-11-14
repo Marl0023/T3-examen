28668b320ea96e60df47ba7b581c11b9aa7c814e
Marl0023 <marlotay2312@gmail.com>
2024-11-14 08:38:03 -0500

    Estructura

--- T3/Program.cs
+++ T3/Program.cs
@@ -1,11 +1,26 @@
 ﻿using T3;
 
-Cita[] citas = new Cita[100];
+Cita[] citas = new Cita[1];
 int cantidadCitas = 0;
 bool salir = false;
-
-Console.WriteLine("Ingrese que opcion desea ingresar primero: ");
-Console.WriteLine("1- Crear Cita");
-Console.WriteLine("2- Listar Cita");
-Console.WriteLine("3- Modificacion masiva en Universidad");
-Console.WriteLine("4.Salir");
+do
+{
+    Console.WriteLine("Ingrese que opcion desea ingresar primero: ");
+    Console.WriteLine("1- Crear Cita");
+    Console.WriteLine("2- Listar Cita");
+    Console.WriteLine("3- Modificacion masiva en Universidad");
+    Console.WriteLine("4.Salir");
+    byte opc = byte.Parse(Console.ReadLine());
+    switch (opc) {
+        case 1:
+            break;
+        case 2:
+            break;
+        case 3:
+            break;
+        case 4:
+            break;
+        default:
+            break;
+    }
+} while (!salir);

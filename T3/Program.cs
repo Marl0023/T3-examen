7c8dbaa7aa7661eae996b98def3beedcf41c09a1
Marl0023 <marlotay2312@gmail.com>
2024-11-14 09:16:27 -0500

    guardado master 2

--- T3/Program.cs
+++ T3/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using T3;
+bool salir = false;
+do
+{
+    
+
+    Console.ForegroundColor = ConsoleColor.Black;
+    Console.BackgroundColor = ConsoleColor.White;
+    Console.WriteLine("\n");
+    Console.WriteLine("*****************************************");
+    Console.WriteLine("1- Crear Cita");
+    Console.WriteLine("2- Listar Cita");
+    Console.WriteLine("3- Modificacion masiva en Universidad");
+    Console.WriteLine("4- Salir");
+    Console.WriteLine("*****************************************");
+    Console.WriteLine("\n");
+    Console.WriteLine("Ingrese que opcion desea ingresar primero: ");
+    byte opc = byte.Parse(Console.ReadLine());
+    Console.ResetColor();
+    switch (opc)
+    {
+        case 1:
+
+            break;
+        case 2:
+            
+            break;
+        case 3:
+            Console.ForegroundColor= ConsoleColor.Yellow;
+            Console.Write("Ingrese el texto a buscar en las universidades: ");
+            string texto = Console.ReadLine();
+            Console.Write("Ingrese el nuevo texto: ");
+            Console.ResetColor();
+            break;
+        case 4:
+            salir = true;
+            break;
+        default:
+            Console.WriteLine("Valor no encontrado, vuelva intentarlo");
+            break;
+    }
+} while (!salir);
+Console.WriteLine("Cerrando Programa...");

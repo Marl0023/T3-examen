8eb309932339bc89496771aeb6545ce9ac670265
Marl0023 <marlotay2312@gmail.com>
2024-11-14 09:10:37 -0500

    Final result 2

--- T3/Program.cs
+++ T3/Program.cs
@@ -7,13 +7,14 @@ do
 {
     Console.WriteLine("\n");
     Console.WriteLine("*****************************************");
-    Console.WriteLine("Ingrese que opcion desea ingresar primero: ");
     Console.WriteLine("1- Crear Cita");
     Console.WriteLine("2- Listar Cita");
     Console.WriteLine("3- Modificacion masiva en Universidad");
     Console.WriteLine("4- Salir");
     Console.WriteLine("*****************************************");
     Console.WriteLine("\n");
+    Console.WriteLine("Ingrese que opcion desea ingresar primero: ");
+    
     byte opc = byte.Parse(Console.ReadLine());
     switch (opc) {
         case 1:
